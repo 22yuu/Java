42a99ecf01e3108534ce6196b7fff9b83a8f6735
22yuu <22yuu@naver.com>
2021-05-17 11:36:15 +0900

    2021/05/17 Java Ch01 수업예제

---
+++ Ch02/src/sub1/VariableTest.java
@@ -0,0 +1,20 @@
+package sub1;
+
+/* 날짜 : 2021/05/17
+ * 이름 : 이진유
+ * 내용 : 자바 변수 실습하기*/
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		int num1;
+		num1 = 1;
+		
+		int num2 = 2;
+		
+		int num3 = num1 + num2;
+		
+		System.out.println("num3 : " + num3);
+	}
+}
